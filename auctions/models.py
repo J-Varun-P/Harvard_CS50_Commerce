eeb5286a06fe2e276eba3b406b1308e38445baae
Varun Pawar Jetling <vj1012@wildcats.unh.edu>
2021-04-05 13:27:24 +0530

    A little change

--- auctions/models.py
+++ auctions/models.py
@@ -9,7 +9,7 @@ class Listings(models.Model):
     title = models.CharField(max_length=64)
     imageurl = models.CharField(max_length=128)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.CharField(max_length=1024)
+    description = models.TextField()
     category = models.CharField(max_length=32)
     name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_listings")
     close = models.CharField(max_length=10, default="false")
